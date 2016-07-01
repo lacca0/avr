af769a8ef49c0ac3b46707b9b5b8ce89c6a698f9
Lena Gerasimenko <laae@mail.ru>
2016-07-01 17:59:43 +0300

    изменен тестовый файл

--- define_test.c
+++ define_test.c
@@ -3,13 +3,13 @@
 
 #include <stdio.h>
 
-#define F_CPU 1000000UL  // 1 MHz\
+#define F_CPU 1000000UL  // 1 MHz
 
 #define multiply(a, b) a*b
 
 
 int main()
 {
-	printf("%u \n %u \n", multiply(4, 3), 5/3);
+	printf("%u \n %u \n", multiply(8, 3), 5/3);
 }
 
