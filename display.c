f7881b78609f026f584f3fe72c6b6a30bf8d2ec4
Lena Gerasimenko <laae@mail.ru>
2017-02-24 22:23:30 +0300

    added library

--- display.c
+++ display.c
@@ -2,16 +2,14 @@
 #include <stdint.h>
 #include <avr/io.h>
 
-#define F_CPU 1000000UL  // 1 MHz
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
-#define RS 0
-#define RW 1
-#define E 2
+#define F_CPU 1000000UL  // 1 MHz
 
 #include <util/delay.h>
 
-#include <avr/interrupt.h>
-#include <avr/sleep.h>
+#include "lib/display.c"
 
 void setup_timers()
 {
@@ -26,98 +24,12 @@ void setup_io()
 	DDRC |= (1 << RS) | (1 << RW) | (1 << E);//вывод
 }
 
-bool receive_busy_flag()
-{
-	DDRA = 0;//ввод
-	PORTA = ~0;
-	PORTC &= ~(1 << RS);
-	PORTC |= (1 << RW);
-	PORTC |= (1 << E);
-	_delay_us(1);
-	bool busy_flag = !!(PINA & (1 << 7));
-	PORTC &= ~(1 << E);
-	return busy_flag;
-}
 
-void start_sending_command()
-{
-	while (receive_busy_flag()) {};
-	DDRA = ~0;//вывод
-	PORTA = 0;
-	PORTC = (1 << E);
-}
 
-void start_sending_command_init()
-{
-	DDRA = ~0;//вывод
-	PORTA = 0;
-	PORTC = (1 << E);
-}
-
-void end_sending()
-{
-	PORTC &= ~(1 << E);
-	PORTA = 0;
-}
-
-void start_sending_data()
-{
-	while (receive_busy_flag()) {}
-	DDRA = ~0;//вывод
-	PORTA = 0;
-	PORTC = (1 << RS) | (1 << E);
-}
-
-void display_visibility_on()
-{
-	start_sending_command();
-	PORTA = 0b00001111;
-	end_sending();
-}
-
-void display_init()
-{
-	_delay_us(15);
-
-	start_sending_command_init();
-	PORTA = 0b00110000;
-	end_sending();
-	_delay_ms(5);
-
-	start_sending_command_init();
-	PORTA = 0b00110000;
-	end_sending();
-	_delay_us(100);
-
-	start_sending_command_init();
-	PORTA = 0b00110000;
-	end_sending();
-	_delay_ms(1);
-
-	start_sending_command_init();
-	PORTA = 0b0011100;//8-bit interface, 2 lines,5x8dots
-	end_sending();
-	_delay_ms(1);
-
-	start_sending_command_init();
-	PORTA = 0b00001000;//display off
-	end_sending();
-	_delay_ms(1);
-
-	start_sending_command_init();
-	PORTA = 0b00000001;//display clear
-	end_sending();
-	_delay_ms(1);
-
-	start_sending_command_init();
-	PORTA = 0b00000110;//increment adress counter, zero shift
-	end_sending();
-	_delay_ms(1);
-
-}
 
 int main()
 {
+
 	/*static uint8_t cycles_counter = 0;
 	setup_io();
 	while(true)
@@ -135,23 +47,24 @@ int main()
 	setup_io();
 	//setup_timers;
 	display_init();
-	display_visibility_on();
+	display_send_command(CMD_DISPLAY_ON_OFF_CTRL | CMD_DOOC_DISPLAY_VISIBLE | CMD_DOOC_CURSOR_VISIBLE );
 
-	start_sending_command();
+	/*start_sending_command();
 	PORTA = 0b10000000;//set DDRAM adress 00
-	end_sending();
+	display_end_sending();*/
 
 	/*start_sending_command();
 	PORTA = 0b00010100;//cursor shift;
-	end_sending();*/
+	display_end_sending();*/
 
-	start_sending_data();//write data
-	PORTA = 0b01001000;//'E'
-	end_sending();
+	char symbol_array[LINE_LENGTH + 1] = "hello Lena";
+	char *array_ptr = symbol_array;
 
-	start_sending_data();//write data
-	PORTA = 0b01001001;//'E'
-	end_sending();
+
+	for (int i = 0; symbol_array[i] != 0; i++)
+	{
+		display_send_character(symbol_array[i]);
+	}
 
 	while(true)
 	{
---
+++ lib/display.c
@@ -0,0 +1,169 @@
+#define RS 0
+#define RW 1
+#define E 2
+#define ERR 3 //PC3 is error signal
+
+#define LINE_LENGTH 16
+
+#define CMD_CLEAR_DISPLAY 0b1
+#define CMD_RETURN_HOME 0b10
+
+#define CMD_ENTRY_MODE_SET 0b100
+#define CMD_EMS_FORWARD 0b10
+#define CMD_EMS_BACKWARD 0
+#define CMD_EMS_SHIFT 0
+#define CMD_EMS_NO_SHIFT 0b1
+
+#define CMD_DISPLAY_ON_OFF_CTRL 0b1000
+#define CMD_DOOC_DISPLAY_VISIBLE 0b100
+#define CMD_DOOC_DISPLAY_NOT_VISIBLE 0
+#define CMD_DOOC_CURSOR_VISIBLE 0b10
+#define CMD_DOOC_CURSOR_NOT_VISIBLE 0
+#define CMD_DOOC_BLINKING_CURSOR_ON 0b1
+
+
+
+
+//PORTA is DB0..7
+//PORTC for RS, RW, E
+
+void panic()//пока здесь, но потом нужно будет перенести.
+{
+	DDRC |= (1 << ERR);
+	cli();
+	while (true)
+	{
+		PORTC |= (1 << ERR);
+		_delay_ms(1000);
+		PORTC &= ~(1 << ERR);
+		_delay_ms(1000);
+	}
+}
+
+bool display_receive_busy_flag()
+{
+	DDRA = 0;//ввод
+	PORTA = ~0;
+	PORTC &= ~(1 << RS);
+	PORTC |= (1 << RW);
+	PORTC |= (1 << E);
+	_delay_us(1);
+	bool busy_flag = !!(PINA & (1 << 7));
+	PORTC &= ~(1 << E);
+	return busy_flag;
+}
+
+void display_start_sending_command()
+{
+	while (display_receive_busy_flag()) {};
+	DDRA = ~0;//вывод
+	PORTA = 0;
+	PORTC &= ~((1 << RS) | (1 << RW));
+	PORTC |= (1 << E);
+}
+
+void display_start_sending_command_init()
+{
+	DDRA = ~0;//вывод
+	PORTA = 0;
+	PORTC &= ~((1 << RS) | (1 << RW));
+	PORTC |= (1 << E);
+}
+
+void display_end_sending()
+{
+	PORTC &= ~(1 << E);
+	PORTA = 0;
+}
+
+void display_send_command(uint8_t command)
+{
+	display_start_sending_command();
+	PORTA = command;
+	display_end_sending();
+}
+
+
+void display_start_sending_data()
+{
+	while (display_receive_busy_flag()) {};
+	DDRA = ~0;//вывод
+	PORTA = 0;
+	PORTC |= (1 << RS);
+	PORTC &= ~(1 << RW);
+	PORTC |= (1 << E);
+}
+
+void display_init()
+{
+	_delay_us(15);
+
+	display_start_sending_command_init();
+	PORTA = 0b00110000;
+	display_end_sending();
+	_delay_ms(5);
+
+	display_start_sending_command_init();
+	PORTA = 0b00110000;
+	display_end_sending();
+	_delay_us(100);
+
+	display_start_sending_command_init();
+	PORTA = 0b00110000;
+	display_end_sending();
+	_delay_ms(1);
+
+	display_start_sending_command_init();
+	PORTA = 0b0011100;//8-bit interface, 2 lines,5x8dots
+	display_end_sending();
+	_delay_ms(1);
+
+	display_start_sending_command_init();
+	PORTA = CMD_DISPLAY_ON_OFF_CTRL |  CMD_DOOC_DISPLAY_NOT_VISIBLE;//display off
+	display_end_sending();
+	_delay_ms(1);
+
+	display_start_sending_command_init();
+	PORTA = CMD_CLEAR_DISPLAY;
+	display_end_sending();
+	_delay_ms(1);
+
+	display_start_sending_command_init();
+	PORTA = 0b00000110;//increment adress counter, zero shift
+	display_end_sending();
+	_delay_ms(1);
+
+}
+
+uint8_t display_get_character_code(char ch)
+{
+	if ((ch >= 'A') && (ch <= 'Z'))
+	{
+		return (0b01000001 + ch - 'A');
+	}
+	else if ((ch >= '0') && (ch <= '9'))
+	{
+		return (0b00110000 + ch - '0');
+	}
+	else if ((ch >= 'a') && (ch <= 'z'))
+	{
+		return (0b01000001 + ch - 'A');
+	}
+	else if (ch == ' ')
+	{
+		return 0b00100000;
+	}
+	else
+	{
+		panic();
+	}
+
+}
+
+void display_send_character(char ch)
+{
+	display_start_sending_data();//write data
+	PORTA = display_get_character_code(ch);
+	display_end_sending();
+}
+
